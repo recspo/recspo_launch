6ff4bb1f022fcca1e8ad6ca9e43af2eab07d8290
Hariharan358 <231401032@rajalakshmi.edu.in>
2026-05-01 09:49:42 +0530

    add url

--- src/pages/Screen.tsx
+++ src/pages/Screen.tsx
@@ -11,7 +11,7 @@ const Screen = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    setJoinUrl(`${window.location.origin}/`);
+    setJoinUrl("https://recspo-launch26.vercel.app/");
   }, []);
 
   useEffect(() => {
